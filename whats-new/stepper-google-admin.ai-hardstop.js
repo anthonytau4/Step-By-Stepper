17481f08f5f5758b20ee92a39c55e00cca0ada77
anthonytau4 <anthonytau4@gmail.com>
2026-06-12 23:15:44 +1200

    Add whats new Google admin shim

---
+++ whats-new/stepper-google-admin.ai-hardstop.js
@@ -0,0 +1,8 @@
+(function(){
+  if (window.__stepperSubpathGoogleAdminShimInstalled) return;
+  window.__stepperSubpathGoogleAdminShimInstalled = true;
+  var script = document.createElement('script');
+  script.src = '../stepper-google-admin.ai-hardstop.js?v=20260612-autosave-cache-1';
+  script.async = false;
+  document.head.appendChild(script);
+})();
